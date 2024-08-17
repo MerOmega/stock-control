5e1e069723bfae4c854713dbc984cf198c327205
MerOmega <insaurralde30@gmail.com>
2024-08-17 14:58:14 -0300

    belongs in model

--- app/Models/Supply.php
+++ app/Models/Supply.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Supply extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'price', 'quantity', 'category_id'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
