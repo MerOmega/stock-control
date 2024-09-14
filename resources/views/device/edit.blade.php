7264c963adfee1bbd89c980a17662c764b418738
MerOmega <insaurralde30@gmail.com>
2024-09-14 13:25:41 -0300

    fix sector

--- resources/views/device/edit.blade.php
+++ resources/views/device/edit.blade.php
@@ -25,7 +25,7 @@
                         Sector
                     </label>
                     <div class="mt-2">
-                        <select name="sector" id="sector" required
+                        <select name="sector_id" id="sector_id" required
                                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 dark:text-gray-100 bg-white dark:bg-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 placeholder:text-gray-400 dark:placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:focus:ring-indigo-500 sm:text-sm sm:leading-6">
                             @foreach($sectors as $sector)
                                 <option value="{{ $sector->id }}">{{ $sector->name }}</option>
